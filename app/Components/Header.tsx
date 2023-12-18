891bca94dd1cb13b54523cb9e1a7ebc82c1e9888
Uditya <workforuditya@gmail.com>
2023-12-18 05:39:46 +0530

    chore:minor ui changes

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -6,18 +6,19 @@ import { ModeToggle } from './ModeToggler'
 
 function Header() {
   return (
-    <header className='flex items-center justify-between'>
-        <Link href='/' className='flex items-center space-x-2'>
+    <header className='flex items-center justify-between py-3 px-2'>
+        <Link href='/' className='flex items-center space-x-4'>
           <Image
-            src="https://www.shareicon.net/data/128x128/2015/09/13/100506_dropbox_512x512.png"
+            src="https://www.shareicon.net/data/128x128/2015/11/08/668675_box_512x512.png"
             alt="logo"
-            className=''
+            className='dark:invert '
+            
             height={40}
             width={40}
           />
         <h1 className='font-bold text-xl'>MY BOX </h1>
         </Link>
-        <div className='flex space-x-2 px-5 '>
+        <div className='flex items-center justify-between space-x-5 px-5 '>
           <ModeToggle/>
           <UserButton afterSignOutUrl='/'/>
           <SignedOut>
